4b2e3f5a875cb2ed15a99ab6afc3bb2b42820edd
DoCongThanhPhuong <bido23082003@gmail.com>
2024-04-22 22:47:56 +0700

    Fetch feed posts

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -118,7 +118,7 @@ function LoginCard() {
             </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
-                loadingText="Submitting"
+                loadingText="Logging in"
                 size="lg"
                 bg={useColorModeValue('gray.600', 'gray.700')}
                 color={'white'}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,49 @@
-import { Button, Flex } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Flex, Spinner, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import Post from '~/components/Post'
+import useShowToast from '~/hooks/useShowToast'
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const showToast = useShowToast()
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch('/api/posts/feed')
+        const data = await res.json()
+        if (data.error) {
+          showToast('Error', data.error, 'error')
+          return
+        }
+
+        setPosts(data)
+      } catch (error) {
+        showToast('Error', error.message, 'error')
+      } finally {
+        setLoading(false)
+      }
+    }
+    getFeedPosts()
+  }, [showToast])
+
   return (
-    <Link to={'/markzuckerberg'}>
-      <Flex w={'full'} justifyContent={'center'}>
-        <Button mx={'auto'}>Visit Profile Page</Button>
-      </Flex>
-    </Link>
+    <>
+      {!loading && posts.length === 0 && (
+        <Text textAlign={'center'}>Follow some users to see the feed!</Text>
+      )}
+
+      {loading && (
+        <Flex justify="center">
+          <Spinner size="xl" />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
+    </>
   )
 }
 
