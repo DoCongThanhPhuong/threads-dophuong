e6dcc3963d725a6d0f05ab3f826338691c48b9cb
DoCongThanhPhuong <bido23082003@gmail.com>
2024-04-22 13:29:27 +0700

    Create post functionality added

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -24,6 +24,7 @@ function LoginCard() {
   const [showPassword, setShowPassword] = useState(false)
   const setAuthScreen = useSetRecoilState(authScreenAtom)
   const setUser = useSetRecoilState(userAtom)
+  const [loading, setLoading] = useState(false)
 
   const [inputs, setInputs] = useState({
     username: '',
@@ -33,6 +34,7 @@ function LoginCard() {
   const showToast = useShowToast()
 
   const handleLogin = async () => {
+    setLoading(true)
     try {
       const res = await fetch('/api/users/login', {
         method: 'POST',
@@ -42,15 +44,18 @@ function LoginCard() {
         body: JSON.stringify(inputs)
       })
       const data = await res.json()
+
       if (data.error) {
         showToast('Error', data.error, 'error')
         return
       }
-      // console.log(data)
+
       localStorage.setItem('user-threads', JSON.stringify(data))
       setUser(data)
     } catch (error) {
       showToast('Error', error, 'error')
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -121,6 +126,7 @@ function LoginCard() {
                   bg: useColorModeValue('gray.700', 'gray.800')
                 }}
                 onClick={handleLogin}
+                isLoading={loading}
               >
                 Login
               </Button>
