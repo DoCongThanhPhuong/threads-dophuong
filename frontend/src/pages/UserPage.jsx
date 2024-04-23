414575c1e26142d8c7149d70c021de7fc81fb699
DoCongThanhPhuong <bido23082003@gmail.com>
2024-04-23 15:21:01 +0700

    Post page added

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -3,33 +3,17 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Post from '~/components/Post'
 import UserHeader from '~/components/UserHeader'
+import useGetUserProfile from '~/hooks/useGetUserProfile'
 import useShowToast from '~/hooks/useShowToast'
 
 function UserPage() {
-  const [user, setUser] = useState(null)
+  const { user, loading } = useGetUserProfile()
   const { username } = useParams()
   const showToast = useShowToast()
-  const [loading, setLoading] = useState(true)
   const [posts, setPosts] = useState([])
   const [fetchingPosts, setFetchingPosts] = useState(true)
 
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const res = await fetch(`/api/users/profile/${username}`)
-        const data = await res.json()
-        if (data.error) {
-          showToast('Error', data.error, 'error')
-          return
-        }
-        setUser(data)
-      } catch (error) {
-        showToast('Error', error.message, 'error')
-      } finally {
-        setLoading(false)
-      }
-    }
-
     const getPosts = async () => {
       setFetchingPosts(true)
       try {
@@ -44,7 +28,6 @@ function UserPage() {
       }
     }
 
-    getUser()
     getPosts()
   }, [username, showToast])
 
